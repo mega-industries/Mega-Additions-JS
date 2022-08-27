2244cfe48a593632183d8abe2e75d4b475be88ec
Mega <110144856+mega2335@users.noreply.github.com>
2022-08-27 08:45:33 -0700

    Update effects.js

--- scripts/libs/effects.js
+++ scripts/libs/effects.js
@@ -1,9 +1,10 @@
+const palette = require("libs/palette")
+
 exports.redFlare = new Effect(40, 100, e => {
-    Draw.color(Color.valueOf("ffc999"));
     Lines.stroke(e.fout() * 2);
     Lines.circle(e.x, e.y, 4 + e.finpow() * 20);
 
-    Draw.color(Color.valueOf("e56666"));
+    Draw.color(palette.red);
     for(let i = 0; i < 2; i++){
         Drawf.tri(e.x, e.y, 6, 40 * e.fout(), i*90+90);
     }
