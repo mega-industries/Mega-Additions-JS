9cf27413114fa64d36ac250f822c70546ddaf501
Mega <110144856+mega2335@users.noreply.github.com>
2022-08-27 08:21:35 -0700

    Create effects.js

---
+++ scripts/effects.js
@@ -0,0 +1,10 @@
+const redFlare = new Effect(40, 100, e => {
+    Draw.color(Color.valueOf("ffc999"));
+    Lines.stroke(e.fout() * 2);
+    Lines.circle(e.x, e.y, 4 + e.finpow() * 20);
+
+    Draw.color(Color.valueOf("e56666"));
+    for(let i = 0; i < 2; i++){
+        Drawf.tri(e.x, e.y, 6, 40 * e.fout(), i*90+90);
+    }
+});
