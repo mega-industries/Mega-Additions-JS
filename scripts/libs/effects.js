6a2ea3f3b233ed3f5925c2a9fcd92d8f17d26f2f
Mega <110144856+mega2335@users.noreply.github.com>
2022-08-27 08:31:15 -0700

    Update effects.js

--- scripts/libs/effects.js
+++ scripts/libs/effects.js
@@ -1,4 +1,4 @@
-const redFlare = new Effect(40, 100, e => {
+exports.redFlare = new Effect(40, 100, e => {
     Draw.color(Color.valueOf("ffc999"));
     Lines.stroke(e.fout() * 2);
     Lines.circle(e.x, e.y, 4 + e.finpow() * 20);
@@ -8,7 +8,3 @@ const redFlare = new Effect(40, 100, e => {
         Drawf.tri(e.x, e.y, 6, 40 * e.fout(), i*90+90);
     }
 });
-
-module.exports = {
-    redFlare: redFlare;
-}
