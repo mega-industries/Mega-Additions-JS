23ae0c76a8ce43d8058a1a2a43af104fc8cadd5d
Mega <110144856+mega2335@users.noreply.github.com>
2022-08-27 09:25:40 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -1,5 +1,28 @@
-const effects = require("libs/effects")
+const effects = require("libs/effects");
+const palette = require("libs/palette");
 
-const angle = extend(PowerTurret, "r-angle", {});
-
-angle.shootType.trailEffect = effects.redFlare
+const angle = extend(PowerTurret, "r-angle", {
+  shootType: extend(EmpBulletType, {
+    damage: 60,
+    areaDamage: 75,
+    areaDamageRadius: 60,
+    radius: 60,
+    timeIncrease: 0,
+    timeDuration: 0,
+    speed: 5,
+    width: 6,
+    height: 6,
+    shrinkY: 0,
+    trailWidth: 3,
+    trailLength: 15,
+    trailColor: palette.red,
+    backColor: palette.red,
+    frontColor: Color.white,
+    hitEffect: effects.angleBlast,
+    despawnEffect: Fx.none,
+    despawnHit: true,
+    lightColor: palette.red,
+    hitColor: palette.red,
+    trailEffect: effects.redFlare,
+  }),
+});
