67552042ae55c0b8da25f6c47e0ed377d95fc828
Mega <110144856+mega2335@users.noreply.github.com>
2022-08-28 14:38:45 -0700

    Update production.js

--- scripts/production.js
+++ scripts/production.js
@@ -1,7 +1,13 @@
 const effects = require("libs/effects");
+const palette = require("libs/palette");
 
 const magDrill = extend(Drill, "magnetic-drill", {
   updateEffect: effects.magneticDrillUpdate,
   updateEFfectChance: 0.05,
   drillEffect: Fx.none,
+});
+
+const scrapFilter = extend(GenericCrafter, "scrap-filter", {
+  updateEffect: Fx.hitLaser,
+  updateEffectChance: 0.04,
 });
