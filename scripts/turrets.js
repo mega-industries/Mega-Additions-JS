1ff4b081d5b9d4de59e7bb9857c6ab11c1a69c35
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-03 09:14:22 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -70,4 +70,4 @@ const sword = extend(ItemTurret, "g-sword", {});
 const battleaxe = extend(ItemTurret, "g-battleaxe", {});
 
 const crack = extend(ItemTurret, "crack", {});
-crack.coolant(new ConsumeLiquid(Liquids.water, 0.06)).update = false;
+crack.coolant = new ConsumeLiquid(Liquids.water, 0.06).update = false;
