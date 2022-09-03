4500869a65c3af718707f09c6aa9319a70fbd7c6
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-03 15:54:50 -0700

    bit turret

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -1,5 +1,7 @@
 const effects = require("libs/effects");
 const palette = require("libs/palette");
+const bitLib = require("libs/turretTypes/BitTurretType");
+const bitBLib = require("libs/bulletTypes/BitBulletType");
 
 const point = extend(ItemTurret, "r-point", {});
 
@@ -70,3 +72,31 @@ const sword = extend(ItemTurret, "g-sword", {});
 const battleaxe = extend(ItemTurret, "g-battleaxe", {});
 
 const crack = extend(ItemTurret, "crack", {});
+
+var size = 8;
+var radius = 40;
+var w = 2.5;
+var l = 3;
+const bitShoot = bitBLib.bitBullet(Color.valueOf("8aa3f4"), Color.valueOf("6974c4"), 0.05, size, false, 8, w, l, 30, radius + 2 * l, 75, 7.5, {
+  speed: 2,
+  lifetime: 90,
+  damage: 12,
+  splashDamage: 34,
+  splashDamageRadius: radius,
+  hitSize: size,
+  homingPower: 0.01,
+  homingRange: 160,
+  knockback: 3,
+  weaveScale: 10,
+  weaveMag: 2
+});
+
+const bit = bitLib.new8BitTurret("b-bit", 8, PowerTurret, PowerTurret.PowerTurretBuild, false, {
+  reloadTime: 70,
+  recoilAmount: 4,
+  inaccuracy: 15,
+  range: 140,
+  powerUse: 1.35,
+  size: 2,
+  shootType: bitShoot
+}, {});
