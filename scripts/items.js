74cb43303267bf3bf7bb90cad21bdbc77b8795cb
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-03 10:14:55 -0700

    Update items.js

--- scripts/items.js
+++ scripts/items.js
@@ -1,8 +1,18 @@
 const palette = require("libs/palette");
+const healingPower = new Stat("healingPower"),
 
 const warpAlloy = extend(Item, "warp-alloy", {
   cost: 1.9,
+  setStats(){
+		this.super$setStats();
+	  this.stats.addPercent(healingPower, 0.14);
+	},
 });
 
-
-const laerite = extend(Liquid, "warp-alloy", {});
+const laerite = extend(Liquid, "laerite", {
+  setStats(){
+		this.super$setStats();
+    this.stats.addPercent(Stat.radioactivity, 0.07);
+	  this.stats.addPercent(healingPower, 0.09);
+	},
+});
