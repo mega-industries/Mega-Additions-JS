96c96cb702a0bd658e9b41dac9d30745b42e1ec0
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-04 11:25:47 -0700

    Update effects.js

--- scripts/libs/effects.js
+++ scripts/libs/effects.js
@@ -84,7 +84,7 @@ exports.magneticDrillUpdate = extend(WaveEffect, {
 
 exports.greenTurretSmoke = extend(ParticleEffect, {
     colorFrom: palette.green,
-    colorTo: palette.green.cpy().a = 0.2,
+    colorTo: Color.valueOf("83f79322")
     sizeFrom: 4,
     sizeTo: 0,
     lifetime: 47,
