7571af13bba82caa12861c2a1f2fed3eb900ee6b
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-07 19:29:03 -0700

    Create bullets.js

---
+++ scripts/libs/bullets.js
@@ -0,0 +1,32 @@
+const effects = require("libs/effects");
+const palette = require("libs/palette");
+
+exports.angleBullet = extend(EmpBulletType, {
+    scaleLife: true,
+    lifetime: 40,
+    damage: 60,
+    areaDamage: 75,
+    areaDamageRadius: 60,
+    radius: 60,
+    timeIncrease: 0,
+    timeDuration: 0,
+    speed: 5,
+    width: 6,
+    height: 6,
+    sprite: "circle-bullet",
+    hitSound: Sounds.plasmaboom,
+    shrinkY: 0,
+    trailWidth: 3,
+    trailLength: 15,
+    trailColor: palette.red,
+    backColor: palette.red,
+    frontColor: Color.white,
+    hitEffect: effects.angleBlast,
+    despawnEffect: Fx.none,
+    despawnHit: true,
+    lightColor: palette.red,
+    hitColor: palette.red,
+    trailEffect: effects.redFlare,
+    trailInterval: 3,
+    trailRotation: true,
+  });
