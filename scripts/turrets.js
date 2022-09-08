76b7a88fdd1b176221f2bf1a804d3e99120656be
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-07 19:29:37 -0700

    Update turrets.js

--- scripts/turrets.js
+++ scripts/turrets.js
@@ -1,5 +1,6 @@
 const effects = require("libs/effects");
 const palette = require("libs/palette");
+const bullets = require("libs/bullets");
 
 const point = extend(ItemTurret, "r-point", {});
 
@@ -7,35 +8,7 @@ const line = extend(PowerTurret, "r-line", {});
 
 const angle = extend(PowerTurret, "r-angle", {
   shootSound: Sounds.malignShoot,
-  shootType: extend(EmpBulletType, {
-    scaleLife: true,
-    lifetime: 40,
-    damage: 60,
-    areaDamage: 75,
-    areaDamageRadius: 60,
-    radius: 60,
-    timeIncrease: 0,
-    timeDuration: 0,
-    speed: 5,
-    width: 6,
-    height: 6,
-    sprite: "circle-bullet",
-    hitSound: Sounds.plasmaboom,
-    shrinkY: 0,
-    trailWidth: 3,
-    trailLength: 15,
-    trailColor: palette.red,
-    backColor: palette.red,
-    frontColor: Color.white,
-    hitEffect: effects.angleBlast,
-    despawnEffect: Fx.none,
-    despawnHit: true,
-    lightColor: palette.red,
-    hitColor: palette.red,
-    trailEffect: effects.redFlare,
-    trailInterval: 3,
-    trailRotation: true,
-  }),
+  shootType: bullets.angleBullet,
 });
 
 const vector = extend(PowerTurret, "r-vector", {});
