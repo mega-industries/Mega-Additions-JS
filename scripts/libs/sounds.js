62e02a479a31d91060514a4b8fc6302032941400
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-10 16:54:20 -0700

    Update sounds.js

--- scripts/libs/sounds.js
+++ scripts/libs/sounds.js
@@ -1,4 +1,4 @@
-exports.blastCharge = Sounds.getSound("blastCharge.ogg");
-exports.blastShoot = Sounds.getSound("blastShoot");
-exports.bitHit = Sounds.getSound("bitHit");
-exports.bitShoot = Sounds.getSound("bitShoot");
+exports.blastCharge = loadSound("blastCharge");
+exports.blastShoot = loadSound("blastShoot");
+exports.bitHit = loadSound("bitHit");
+exports.bitShoot = loadSound("bitShoot");
