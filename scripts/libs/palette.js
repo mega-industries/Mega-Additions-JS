3ba37ffe1d0556dcd253f6b2db6eba8552a1d74b
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-09 17:32:31 -0700

    Update palette.js

--- scripts/libs/palette.js
+++ scripts/libs/palette.js
@@ -3,4 +3,4 @@ exports.blue = Color.valueOf("8aa3f4");
 exports.green = Color.valueOf("83f793");
 exports.lightRed = Color.valueOf("ff7272");
 exports.black = Color.valueOf("000000");
-exports.darkGray = Color.valueOf("4d4e5899");
+exports.darkGray = Color.valueOf("4d4e58");
