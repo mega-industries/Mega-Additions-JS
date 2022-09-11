457b5fc66467fcf3d1d8780afbf24b6ccd2b1ba2
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-11 07:07:08 -0700

    worth a shot i guess

--- scripts/libs/effects.js
+++ scripts/libs/effects.js
@@ -162,21 +162,21 @@ exports.photonTrail = new Effect(20, e => {
 });
 
 exports.bitBurst = new Effect(30, e => {
-	let size = 21;
+	let size = 21 * e.fin();
 	Draw.color(Pal.lancerLaser);
 	Lines.stroke(2.5 * e.fout());
 	
-	Lines.line(e.x, e.y, e.x - size * e.fin(), e.y);
-	Lines.line(e.x, e.y, e.x + size * e.fin(), e.y);
+	Lines.line(e.x, e.y, e.x - size, e.y);
+	Lines.line(e.x, e.y, e.x + size, e.y);
 	
-	Lines.line(e.x, e.y, e.x, e.y - size * e.fin());
-	Lines.line(e.x, e.y, e.x, e.y + size * e.fin());
+	Lines.line(e.x, e.y, e.x, e.y - size);
+	Lines.line(e.x, e.y, e.x, e.y + size);
 	
-	Lines.line(e.x, e.y, e.x - size * e.fin(), e.y - size * e.fin());
-	Lines.line(e.x, e.y, e.x + size * e.fin(), e.y + size * e.fin());
+	Lines.line(e.x, e.y, e.x - size, e.y - size);
+	Lines.line(e.x, e.y, e.x + size, e.y + size);
 	
-	Lines.line(e.x, e.y, e.x - size * e.fin(), e.y + size * e.fin());
-	Lines.line(e.x, e.y, e.x + size * e.fin(), e.y - size * e.fin());
+	Lines.line(e.x, e.y, e.x - size, e.y + size);
+	Lines.line(e.x, e.y, e.x + size, e.y - size);
 });
 
 exports.bitTrail = new Effect(75, e => {
@@ -195,24 +195,42 @@ exports.whiteShoot = new Effect(37, e => {
 });
 
 exports.whiteCharge = new Effect(44, e => {
-	let size = 39;
+	let size = 39 * e.fout();
 	Draw.color(palette.darkGray, Color.white, e.fin());
 	Lines.stroke(2.5 * e.fin());
 	
-	Lines.line(e.x, e.y, e.x - size * e.fout(), e.y);
-	Lines.line(e.x, e.y, e.x + size * e.fout(), e.y);
+	Lines.line(e.x, e.y, e.x - size, e.y);
+	Lines.line(e.x, e.y, e.x + size, e.y);
 	
-	Lines.line(e.x, e.y, e.x, e.y - size * e.fout());
-	Lines.line(e.x, e.y, e.x, e.y + size * e.fout());
+	Lines.line(e.x, e.y, e.x, e.y - size);
+	Lines.line(e.x, e.y, e.x, e.y + size);
 	
-	Lines.line(e.x, e.y, e.x - size * e.fout(), e.y - size * e.fout());
-	Lines.line(e.x, e.y, e.x + size * e.fout(), e.y + size * e.fout());
+	Lines.line(e.x, e.y, e.x - size, e.y - size);
+	Lines.line(e.x, e.y, e.x + size, e.y + size);
 	
-	Lines.line(e.x, e.y, e.x - size * e.fout(), e.y + size * e.fout());
-	Lines.line(e.x, e.y, e.x + size * e.fout(), e.y - size * e.fout());
+	Lines.line(e.x, e.y, e.x - size, e.y + size);
+	Lines.line(e.x, e.y, e.x + size, e.y - size);
 });
 
 exports.spaceTrail = new Effect(20, e => {
 	Draw.color(Color.white, palette.darkGray, e.fin());
 	Drawf.tri(e.x, e.y, 8 * e.fout(), 8 * e.fout(), e.rotation);
+});
+
+exports.smallImplode = new Effect(35, e => {
+	Draw.color(Color.white, palette.black, e.fin());
+	Lines.stroke(2 * e.fin());
+	Lines.circle(e.x, e.y, 15 - e.finpow() * 4);
+});
+
+exports.largeImplode = new Effect(55, e => {
+	Draw.color(Color.white, palette.black, e.fin());
+	Lines.stroke(3 * e.fin());
+	Lines.circle(e.x, e.y, 45 - e.finpow() * 4);
+	
+	const hl = new Floatc2({get: function(x, y){
+		Fill.poly(e.x + x, e.y + y, 4, e.fout() * 4, e.rotation);
+	}});
+	
+	Angles.randLenVectors(e.id, 5, 0 - e.finpow() * 20.0, e.rotation, 180.0, hl);
 });
