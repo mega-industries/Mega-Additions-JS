6502e801e5e1ef23c3199e25567ac03b6cb82126
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-10 17:18:32 -0700

    Delete sounds.js

--- scripts/libs/sounds.js
+++
@@ -1,4 +0,0 @@
-exports.blastCharge = FileTree.loadSound("blastCharge");
-exports.blastShoot = loadSound("blastShoot");
-exports.bitHit = loadSound("bitHit");
-exports.bitShoot = loadSound("bitShoot");
