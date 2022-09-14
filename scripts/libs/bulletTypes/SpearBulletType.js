e999b650f3c2d3a90ac7792516fdd08b63ca6055
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-13 20:11:04 -0700

    spear

---
+++ scripts/libs/bulletTypes/SpearBulletType.js
@@ -0,0 +1,35 @@
+exports.SpearBulletType(color, width, height, sideWidth, sideLength sideAngle, obj){
+  const shoot = extend(ParticleEffect, {
+    line: true,
+    lenFrom: 4,
+    lenTo: 0,
+    strokeFrom: 1.4,
+    strokeTo: 0,
+    colorFrom: Color.white,
+    colorTo: color,
+    cone: 30,
+    length: 12,
+    lifetime: 50,
+    particles: 8
+  });
+  
+  if(obj == undefined) obj = {};
+  obj = Object.assign({
+    draw(b){
+      if(!b) return;
+      
+      Draw.color(color);
+      Draw.z(Layer.bullet);
+      
+      Drawf.tri(e.x, e.y, width, height, e.rotation)
+      Drawf.tri(e.x, e.y, sideWidth, sideLength, e.rotation - sideAngle)
+      Drawf.tri(e.x, e.y, sideWidth, sideLength, e.rotation + sideAngle)
+    },
+    hittable: false,
+    absorbable: false,
+    shootEffect: shoot,
+  }, obj);
+  
+  const spear = extend(BasicBulletType, obj);
+  return spear;
+});
