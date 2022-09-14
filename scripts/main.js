a8b1429efae54a330c6779057993665e0413f1de
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-13 20:55:11 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
+const bullets = require("libs/bullets");
+
+const testBlock = extend(PowerTurret, "test", {
+  shootType: bullets.polarisSpear
+});
+
 require("turrets");
 require("production");
 require("items");
