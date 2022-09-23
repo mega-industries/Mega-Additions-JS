7fd1d3caec4287b2ccec4db8fcc397d2adbd4785
Mega <110144856+mega2335@users.noreply.github.com>
2022-09-23 14:05:57 -0700

    Update debug.js

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,5 +1,14 @@
 const bullets = require("libs/bullets");
+const stats = require("libs/stats");
 
 const testBlock = extend(PowerTurret, "test", {
   shootType: Bullets.placeholder,
+});
+
+const anuken = extend(PowerTurret, "op-anuken", {
+  setStats(){
+    this.super$setStats()
+    this.stats.add(stats.whatTheFork)
+  },
+  shootType: bullets.FORK_YOU
 });
